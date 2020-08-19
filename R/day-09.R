9f0c9c7ba0e1003f19aa5f9ecb4a96aa20ac8e57
Meagan Brown <meaganbrown15@gmail.com>
2020-08-19 16:49:25 -0700

    c

---
+++ R/day-09.R
@@ -0,0 +1,26 @@
+#Meagan Brown
+#August 19 2020
+#Ex 10
+
+library(USAboundaries)
+library(tidyverse)
+library(sf)
+
+states_og <- us_states()
+
+us <- states_og %>%
+  filter(!(name %in% c('Puerto Rico', 'Alaska', 'Hawaii')))
+
+state = st_cast(us, 'MULTILINESTRING') %>%
+  select(geometry)
+
+plot(state$geometry, col = 'green')
+state
+
+
+states_border <- st_union(us$geometry) %>%
+  st_cast('MULTILINESTRING')
+
+
+
+
